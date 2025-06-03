cfdd19e78447afb403ccbd07225f23fd10095a7a
issy-inglis@hotmail.co.uk <issy-inglis@hotmail.co.uk>
2025-06-03 15:23:49 +0100

    initialise prisma and connect to db

---
+++ server/prisma/migrations/20250603135631_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE `Event` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
