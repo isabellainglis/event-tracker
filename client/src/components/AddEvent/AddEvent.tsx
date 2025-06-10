6dda983fe4235518fe79539ee22b72e74a13f5fd
issy-inglis@hotmail.co.uk <issy-inglis@hotmail.co.uk>
2025-06-10 16:43:40 +0100

    create modal and new event form

--- client/src/components/AddEvent/AddEvent.tsx
+++ client/src/components/AddEvent/AddEvent.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./AddEvent.css";
 import { useMutation, gql } from "@apollo/client";
 
@@ -11,26 +12,53 @@ const ADD_EVENT = gql`
 `;
 
 export default function AddEvent() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [addEvent] = useMutation(ADD_EVENT);
 
   return (
     <section className="add-event">
-      <div className="header__button-container">
+      <div className="add-event__button-container">
         <button
-          className="header__add-event-btn"
-          onClick={() =>
-            addEvent({
-              variables: {
-                name: "Festival",
-                date: "2025-10-10",
-                location: "NYC",
-              },
-            })
-          }
+          className="add-event__btn"
+          onClick={() => setIsModalOpen(!isModalOpen)}
         >
-          Add Event
+          New Event
         </button>
       </div>
+      {isModalOpen ? (
+        <div className="add-event__modal">
+          <form className="add-event__form">
+            <label className="add-event__label">
+              Event name:
+              <input type="text" name="name" className="add-event__input" />
+            </label>
+            <label className="add-event__label">
+              Date:
+              <input type="text" name="date" className="add-event__input" />
+            </label>
+            <label className="add-event__label">
+              Location:
+              <input type="text" name="location" className="add-event__input" />
+            </label>
+            <button
+              className="add-event__modal-btn"
+              onClick={() =>
+                addEvent({
+                  variables: {
+                    name: "Festival",
+                    date: "2025-10-10",
+                    location: "NYC",
+                  },
+                })
+              }
+            >
+              Add Event
+            </button>
+          </form>
+        </div>
+      ) : (
+        ""
+      )}
     </section>
   );
 }
